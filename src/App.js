ba3a830479fa844d70b86aec6948fd2f1bfef1f7
lamoncso <laukomonika@gmail.com>
2023-07-31 19:49:06 +0200

    Change text

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
         </a>{" "}
         and{" "}
         <a href="https://app.netlify.com" target="_blank" rel="noreferrer">
-          hosted on Netlify. testtest
+          hosted on Netlify.
         </a>
       </footer>
     </div>
