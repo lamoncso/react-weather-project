3a90411438700d2b748baccd661c07a36616fd1e
lamoncso <laukomonika@gmail.com>
2023-08-03 21:55:23 +0200

    Change minutes condition

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -14,7 +14,7 @@ export default function FormattedDate(props){
     let hours=props.date.getHours();
 
      if (hours < 10) {
-       hours = `0${minutes}`;
+       hours = `0${hours}`;
      }
 
     let minutes=props.date.getMinutes();
