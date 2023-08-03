77deb8883e74b0cdd6ed0414b6064eef2984bd16
lamoncso <laukomonika@gmail.com>
2023-08-03 19:30:50 +0200

    Add FormattedDate component
    {day}, {hours}:{minutes}

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate"
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -9,7 +10,7 @@ const [weatherData, setWeatherData]=useState({ready:false});
         console.log(response);
        setWeatherData({
          ready: true,
-         date: "Di, 13:00",
+         date: new Date(response.data.dt*1000),
          description: response.data.weather[0].description,
          iconUrl:
            "https://th.bing.com/th/id/R.770b805d5c99c7931366c2e84e88f251?rik=khgO%2bY1Hh3BT9w&riu=http%3a%2f%2fpurepng.com%2fpublic%2fuploads%2flarge%2fpurepng.com-weather-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596142qx4ep.png&ehk=6msbAydV7X6D4bO8zvLC664aXwKOdBU17dwrHcKxaAg%3d&risl=&pid=ImgRaw&r=0",
@@ -46,7 +47,7 @@ if (weatherData.ready){
       <div className="date">
         <ul>
           <li className="description text-capitalize">{weatherData.description}</li>
-          <li className="today">{weatherData.date}</li>
+          <li className="today"><FormattedDate date={weatherData.date} /></li>
         </ul>
       </div>
       <div className="row">
